03a14481bcf913c5cd760e6728f02af927f2b4d1
Tianyu Guo <guoty3310@gmail.com>
2016-01-10 23:01:33 -0500

    Delete sketch_mar01a.ino

--- RN4020 BLE Setup/sketch_mar01a/sketch_mar01a.ino
+++
@@ -1,9 +0,0 @@
-void setup(){
-    Serial.begin(115200);
-    delay(1000);
-    Serial.println("SR,20000000"); //Set device in peripheral mode and set turn on auto advertising
-    Serial.println("R,1"); //Reboot
-    delay(500);
-    Serial.println("A"); //Start advertising
-}
-void loop(){}
